f714668b5622c1466b136290eefc65beae026ad0
taswar <taswar@zeytinsoft.com>
2014-10-26 23:02:01 -0400

    Bruce Lee Demo Code

---
+++ BruceLeeKickCount/BruceLeeKickCount/BruceLeeKickHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace BruceLeeKickCount
+{
+    [HubName("bruceLeeKickHub")]
+    public class BruceLeeKickHub : Hub
+    {
+        private static int _kick;
+
+        public void DropKick()
+        {
+            _kick++;
+            Clients.All.onKicking(_kick);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _kick--;
+            Clients.All.onKicking(_kick);
+            Clients.User("taswar").onKicking(_kick);
+                            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
